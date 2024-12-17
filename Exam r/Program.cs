dd8f0649d40dceb8f7a31d53bfac6c6cf6f902d8
ROAA-TURKI <roaa.turky.alhazmi@gmail.com>
2024-12-17 18:52:53 +0300

    .
    .

---
+++ Exam r/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program{
+    public static void Main(string[] array){
+        int sec = 50;
+        Console.WriteLine("Enter number :");
+
+int g = Convert.ToInt32(Console.ReadLine());
+if (g<sec)
+
+{
+    Console.WriteLine("tolo");
+}
+}
+
+
+
+
+}
